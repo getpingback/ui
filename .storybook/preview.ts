1c25398474184932855a9f0e598b4a37fae33c1d
Pedro Ladeira <pmladeira@live.com>
2023-10-25 15:09:45 -0300

    docs: add some development docs

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,17 @@ import "../src/styles/globals.css";
 
 const preview: Preview = {
   parameters: {
+    options: {
+      storySort: {
+        order: [
+          "Overview",
+          ["Introduction", "Getting started", "Changelog"],
+          "Development",
+          ["Contributing", "Code of conduct", "Release process"],
+          "Components",
+        ],
+      },
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
