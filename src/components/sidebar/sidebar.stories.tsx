07c135560b9d07bde48a2f7f942e766d3d3e0e34
Jéss Martins <81935519+Jessmartins91@users.noreply.github.com>
2023-10-31 15:36:10 -0300

    feat: create sidebar , sidebarheader , sidebarFooter , sidebarContent

---
+++ src/components/sidebar/sidebar.stories.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Sidebar, SideBarHeader, SideBarFooter , SideBarContent } from './sidebar';
+
+const meta = {
+  title: 'Components/Sidebar',
+  component: Sidebar,
+  parameters: {
+    layout: 'centered',
+  },
+
+  tags: ['autodocs'],
+
+  argTypes: {},
+} satisfies Meta<typeof Sidebar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className='flex justify-center'>
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen}>
+        <SideBarContent>
+        <SideBarHeader>Header</SideBarHeader>
+        <div className='h-[40px]'>Dashboard</div>
+        <div className='h-[40px]'>Seetings</div>
+        <div className='h-[40px]'>Create</div>
+        <div className='h-[40px]'>Measure</div>
+        </SideBarContent>
+        
+        <SideBarFooter>Footer</SideBarFooter>
+      </Sidebar>
+      <button className='p-2 border rounded-lg' onClick={() => setIsOpen(!isOpen)}>
+        Click me
+      </button>
+    </div>
+  );
+};
