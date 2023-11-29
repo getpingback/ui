167a915b2d701c600b2c090d239d884b6323d442
Pedro Ladeira <pmladeira@live.com>
2023-11-29 13:27:27 -0300

    chore: add inline svg

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,29 +1,25 @@
-import * as React from 'react';
-import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu';
-import * as AccordionPrimitive from '@radix-ui/react-accordion';
-import { cva, type VariantProps } from 'class-variance-authority';
-import { ChevronDownIcon } from '@stash-ui/regular-icons';
-// const subItem1SVG = require('../../assets/images/sub-item1.svg');
-// const subItem2SVG = require('../../assets/images/sub-item2.svg');
-// const subItem3SVG = require('../../assets/images/sub-item3.svg');
-// const subItem4SVG = require('../../assets/images/sub-item4.svg');
+import * as React from "react";
+import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { cva, type VariantProps } from "class-variance-authority";
+import { ChevronDownIcon } from "@stash-ui/regular-icons";
 
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
 
 const navigationVariants = cva(
-  'h-[40px] inline-flex  items-center justify-between w-full p-3 rounded-lg cursor-pointer text-sm font-normal',
+  "h-[40px] inline-flex  items-center justify-between w-full p-3 rounded-lg cursor-pointer text-sm font-normal",
   {
     variants: {
       variant: {
         highlighted:
-          'shadow shadow-[0px_0px_0px_3px_rgba(14,159,110,0.12)] hover:bg-list-highlighted text-success-foreground',
+          "shadow shadow-[0px_0px_0px_3px_rgba(14,159,110,0.12)] hover:bg-list-highlighted text-success-foreground",
         default:
-          'text-tertiary-foreground hover:text-active-foreground hover:bg-list-actived',
-        disabled: 'text-tertiary-foreground opacity-[0.45] cursor-not-allowed',
+          "text-tertiary-foreground hover:text-active-foreground hover:bg-list-actived",
+        disabled: "text-tertiary-foreground opacity-[0.45] cursor-not-allowed",
       },
     },
     defaultVariants: {
-      variant: 'default',
+      variant: "default",
     },
   }
 );
@@ -49,10 +45,10 @@ function NavigationLink({
           className,
           isActive &&
             !variant &&
-            'text-active-foreground bg-active-menu font-semibold',
+            "text-active-foreground bg-active-menu font-semibold",
           isActive &&
-            variant === 'highlighted' &&
-            'text-success-foreground bg-list-highlighted font-semibold'
+            variant === "highlighted" &&
+            "text-success-foreground bg-list-highlighted font-semibold"
         )}
         {...props}
       />
@@ -81,10 +77,10 @@ function NavigationItem({
           className,
           isActive &&
             !variant &&
-            'text-active-foreground bg-active-menu font-semibold',
+            "text-active-foreground bg-active-menu font-semibold",
           isActive &&
-            variant === 'highlighted' &&
-            'text-success-foreground bg-list-highlighted font-semibold'
+            variant === "highlighted" &&
+            "text-success-foreground bg-list-highlighted font-semibold"
         )}
         {...props}
       />
@@ -94,7 +90,7 @@ function NavigationItem({
 
 export interface NavigationSubItemProps
   extends React.ComponentProps<typeof NavigationMenuPrimitive.Link> {
-  position?: 'first' | 'middle' | 'last' | 'only';
+  position?: "first" | "middle" | "last" | "only";
 }
 
 function NavigationSubItem({
@@ -103,19 +99,97 @@ function NavigationSubItem({
   ...props
 }: NavigationSubItemProps) {
   const renderLeftIconPosition = (position: string) => {
-    // if (position === 'first') {
-    //   return <img src={subItem1SVG} alt='dot-icon' data-testid='first-dot' />;
-    // }
-    // if (position === 'last') {
-    //   return <img src={subItem3SVG} alt='dot-icon' data-testid='last-dot' />;
-    // }
-    // if (position === 'only') {
-    //   return <img src={subItem4SVG} alt='dot-icon' data-testid='only-dot' />;
-    // }
+    if (position === "first") {
+      return (
+        <svg
+          width='40'
+          height='40'
+          viewBox='0 0 40 40'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+          data-testid='first-dot'
+        >
+          <path
+            d='M17 20C17 18.3431 18.3431 17 20 17C21.6569 17 23 18.3431 23 20C23 21.6569 21.6569 23 20 23C18.3431 23 17 21.6569 17 20Z'
+            fill='#71717A'
+          />
+          <path
+            fill-rule='evenodd'
+            clip-rule='evenodd'
+            d='M20.5 23L20.5 40L19.5 40L19.5 23L20.5 23Z'
+            fill='#71717A'
+          />
+        </svg>
+      );
+    }
+    if (position === "last") {
+      return (
+        <svg
+          width='40'
+          height='40'
+          viewBox='0 0 40 40'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+          data-testid='last-dot'
+        >
+          <path
+            fill-rule='evenodd'
+            clip-rule='evenodd'
+            d='M20.5 -4.37114e-08L20.5 17L19.5 17L19.5 0L20.5 -4.37114e-08Z'
+            fill='#71717A'
+          />
+          <path
+            d='M17 20C17 18.3431 18.3431 17 20 17C21.6569 17 23 18.3431 23 20C23 21.6569 21.6569 23 20 23C18.3431 23 17 21.6569 17 20Z'
+            fill='#71717A'
+          />
+        </svg>
+      );
+    }
+    if (position === "only") {
+      return (
+        <svg
+          width='40'
+          height='40'
+          viewBox='0 0 40 40'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+          data-testid='only-dot'
+        >
+          <path
+            d='M17 20C17 18.3431 18.3431 17 20 17C21.6569 17 23 18.3431 23 20C23 21.6569 21.6569 23 20 23C18.3431 23 17 21.6569 17 20Z'
+            fill='#71717A'
+          />
+        </svg>
+      );
+    }
 
-    // return <img src={subItem2SVG} alt='dot-icon' data-testid='middle' />;
-
-    return null;
+    return (
+      <svg
+        width='40'
+        height='40'
+        viewBox='0 0 40 40'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+        data-testid='middle'
+      >
+        <path
+          fill-rule='evenodd'
+          clip-rule='evenodd'
+          d='M20.5 -4.37114e-08L20.5 17L19.5 17L19.5 0L20.5 -4.37114e-08Z'
+          fill='#71717A'
+        />
+        <path
+          d='M17 20C17 18.3431 18.3431 17 20 17C21.6569 17 23 18.3431 23 20C23 21.6569 21.6569 23 20 23C18.3431 23 17 21.6569 17 20Z'
+          fill='#71717A'
+        />
+        <path
+          fill-rule='evenodd'
+          clip-rule='evenodd'
+          d='M20.5 23L20.5 40L19.5 40L19.5 23L20.5 23Z'
+          fill='#71717A'
+        />
+      </svg>
+    );
   };
 
   return (
@@ -123,14 +197,14 @@ function NavigationSubItem({
       <NavigationMenuPrimitive.Link
         data-testid='navigation-sub-item'
         className={cn(
-          'h-[40px] flex items-center flex-start w-full px-3 cursor-pointer text-xs font-normal text-primary hover:bg-list-hover transition duration-300 ease-in-out',
+          "h-[40px] flex items-center flex-start w-full px-3 cursor-pointer text-xs font-normal text-primary hover:bg-list-hover transition duration-300 ease-in-out",
           className
         )}
         {...props}
       >
         <div
           className={cn(
-            'w-full opacity-60 hover:opacity-100 flex items-center text-tertiary-foreground flex-start transition-all duration-300 z-9'
+            "w-full opacity-60 hover:opacity-100 flex items-center text-tertiary-foreground flex-start transition-all duration-300 z-9"
           )}
         >
           {position && renderLeftIconPosition(position)}
@@ -142,7 +216,7 @@ function NavigationSubItem({
 }
 
 interface NavigationTriggerProps
-  extends Omit<AccordionPrimitive.AccordionItemProps, 'value'> {
+  extends Omit<AccordionPrimitive.AccordionItemProps, "value"> {
   items: { label: string; href: string }[];
 }
 
@@ -153,10 +227,10 @@ function NavigationTrigger({
   ...props
 }: NavigationTriggerProps) {
   const handlePosition = (itemIndex: number, itemsLength: number) => {
-    if (itemsLength === 1) return 'only';
-    else if (itemIndex === 0 && itemsLength > 1) return 'first';
-    else if (itemIndex === itemsLength - 1) return 'last';
-    else return 'middle';
+    if (itemsLength === 1) return "only";
+    else if (itemIndex === 0 && itemsLength > 1) return "first";
+    else if (itemIndex === itemsLength - 1) return "last";
+    else return "middle";
   };
 
   return (
@@ -173,7 +247,7 @@ function NavigationTrigger({
       >
         <AccordionPrimitive.Trigger
           className={cn(
-            'h-[40px] inline-flex items-center justify-between w-full cursor-pointer text-sm font-normal text-tertiary-foreground  p-3 rounded-lg  hover:text-active-foreground [&[data-state=open]>svg]:rotate-180 [&[data-state=open]]:bg-active-menu [&[data-state=open]]:text-active-foreground [&[data-state=open]]:font-semibold hover:text-opacity-100 hover:bg-list-actived transition duration-400 ease-in-out',
+            "h-[40px] inline-flex items-center justify-between w-full cursor-pointer text-sm font-normal text-tertiary-foreground  p-3 rounded-lg  hover:text-active-foreground [&[data-state=open]>svg]:rotate-180 [&[data-state=open]]:bg-active-menu [&[data-state=open]]:text-active-foreground [&[data-state=open]]:font-semibold hover:text-opacity-100 hover:bg-list-actived transition duration-400 ease-in-out",
             className
           )}
         >
@@ -182,7 +256,7 @@ function NavigationTrigger({
         </AccordionPrimitive.Trigger>
         <AccordionPrimitive.Content
           className={cn(
-            'w-full overflow-hidden data-[state=open]:animate-slide-up data-[state=closed]:animate-slide-down'
+            "w-full overflow-hidden data-[state=open]:animate-slide-up data-[state=closed]:animate-slide-down"
           )}
         >
           {items.map((item, index) => (
@@ -208,7 +282,7 @@ function NavigationList({ ...props }: NavigationListProps) {
     <NavigationMenuPrimitive.Root>
       <NavigationMenuPrimitive.List
         className={cn(
-          'group flex list-none flex-col gap-1 w-full overflow-hidden'
+          "group flex list-none flex-col gap-1 w-full overflow-hidden"
         )}
         {...props}
       />
