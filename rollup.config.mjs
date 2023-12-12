05d82b30e38bffa6d53509b63a6e33ce9fde2975
Jéss Martins <81935519+Jessmartins91@users.noreply.github.com>
2023-12-12 15:57:31 -0300

    fix: change in rollup.config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,28 +1,28 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
-import dts from "rollup-plugin-dts";
-import copy from "rollup-plugin-copy";
-import svg from "rollup-plugin-svg";
-import sourcemaps from "rollup-plugin-sourcemaps";
-import del from "rollup-plugin-delete";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import dts from 'rollup-plugin-dts';
+import copy from 'rollup-plugin-copy';
+import svg from 'rollup-plugin-svg';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import del from 'rollup-plugin-delete';
 
-import packageJson from "./package.json" assert { type: "json" };
+import packageJson from './package.json' assert { type: 'json' };
 
 const rollupConfig = [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
-        sourcemap: "inline",
+        format: 'cjs',
+        sourcemap: 'inline',
       },
       {
         file: packageJson.module,
-        format: "esm",
-        sourcemap: "inline",
+        format: 'esm',
+        sourcemap: 'inline',
       },
     ],
     plugins: [
@@ -31,38 +31,39 @@ const rollupConfig = [
       commonjs(),
       postcss({
         config: {
-          path: "./postcss.config.js",
+          path: './postcss.config.js',
         },
-        extensions: [".css"],
+        extensions: ['.css'],
         minimize: true,
         inject: {
-          insertAt: "top",
+          insertAt: 'top',
         },
       }),
       copy({
         targets: [
           {
-            src: "./tailwind.config.js",
-            dest: "./dist",
+            src: './tailwind.config.js',
+            dest: './dist',
           },
           {
-            src: "./src/styles/globals.css",
-            dest: "./dist",
+            src: './src/styles/globals.css',
+            dest: './dist',
           },
         ],
       }),
       typescript({
         sourceMap: true,
         inlineSources: true,
-        tsconfig: "./tsconfig.json",
+        tsconfig: './tsconfig.json',
       }),
       sourcemaps(),
     ],
+    external: ['react', 'react-dom'],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [dts(), del({ targets: "dist/types", hook: "buildEnd" })],
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts(), del({ targets: 'dist/types', hook: 'buildEnd' })],
   },
 ];
 
