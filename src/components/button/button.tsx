70fe84689ea0dbc3ea69f3c5fe89a508c98faa66
Pedro Ladeira <pmladeira@live.com>
2024-05-07 15:32:45 -0300

    feat(date-picker): create component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,7 +4,6 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
-// bg-badge-pending text-badge-pending-foreground
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 transition-all duration-200 ease-in-out",
   {
