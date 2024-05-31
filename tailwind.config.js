3941feb8a4192255e59fa99f969768cd99f78226
Pedro Ladeira <pmladeira@live.com>
2024-05-31 15:23:01 -0300

    fix: change accordion name animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -143,11 +143,11 @@ const config = {
         "active-menu": "var(--palette-gradiente-active_menu)",
       },
       keyframes: {
-        slideUp: {
+        accordionSlideUp: {
           from: { height: 0 },
           to: { height: "var(--radix-collapsible-content-height)" },
         },
-        slideDown: {
+        accordionSlideDown: {
           from: { height: "var(--radix-collapsible-content-height)" },
           to: { height: 0 },
         },
@@ -167,8 +167,8 @@ const config = {
         },
       },
       animation: {
-        "slide-up": "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1) forwards",
-        "slide-down": "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1) forwards",
+        "slide-up": "accordionSlideUp 300ms cubic-bezier(0.87, 0, 0.13, 1) forwards",
+        "slide-down": "accordionSlideDown 300ms cubic-bezier(0.87, 0, 0.13, 1) forwards",
         "slide-left": "slideLeft 300ms cubic-bezier(0.83, 0, 0.17, 1)",
         "slide-right": "slideRight 300ms cubic-bezier(0.83, 0, 0.17, 1)",
       },
