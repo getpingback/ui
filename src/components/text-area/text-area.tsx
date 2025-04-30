dc8f725b2947f931f856a957df7b56c3725ea2d6
Jéss Martins <81935519+Jessmartins91@users.noreply.github.com>
2025-04-30 11:28:54 -0300

    fix: add ref to textfield and textarea

--- src/components/text-area/text-area.tsx
+++ src/components/text-area/text-area.tsx
@@ -1,4 +1,4 @@
-import React, { TextareaHTMLAttributes } from 'react';
+import React, { TextareaHTMLAttributes, forwardRef } from 'react';
 import { AsteriskIcon } from '@stash-ui/solid-icons';
 import { cn } from '@/lib/utils';
 
@@ -10,47 +10,40 @@ interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-const TextArea = ({
-  label,
-  placeholder,
-  value,
-  onChange,
-  error,
-  helperText,
-  disabled,
-  className,
-  defaultHeight = 112,
-  required,
-  ...props
-}: TextAreaProps) => {
-  return (
-    <div className={cn('flex flex-col gap-1', className)}>
-      <label className={cn('text-tertiary-foreground text-xs font-semibold leading-4 flex items-center', { 'opacity-85': disabled })}>
-        {label}
-        {required && <AsteriskIcon color="#52525B" width={16} height={16} opacity={0.45} />}
-      </label>
-      <div className="relative w-full">
-        <textarea
-          className={cn(
-            'w-full rounded-lg border h-10 px-3 py-2 text-sm leading-none text-tertiary-foreground font-normal transition-all outline-none',
-            'placeholder:text-tertiary-foreground placeholder:opacity-65 placeholder:font-normal placeholder:text-sm',
-            { 'border-divider-error focus:border-divider-error focus:shadow-text-field-error': error },
-            { 'border-divider hover:border-divider-active focus:border-divider-highlighted focus:shadow-text-field': !error },
-            { 'border-divider-disabled hover:border-divider-disabled cursor-not-allowed': disabled },
-            'resize-none'
-          )}
-          style={{ height: defaultHeight }}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          disabled={disabled}
-          {...props}
-        />
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ label, placeholder, value, onChange, error, helperText, disabled, className, defaultHeight = 112, required, ...props }, ref) => {
+    return (
+      <div className={cn('flex flex-col gap-1', className)}>
+        <label className={cn('text-tertiary-foreground text-xs font-semibold leading-4 flex items-center', { 'opacity-85': disabled })}>
+          {label}
+          {required && <AsteriskIcon color="#52525B" width={16} height={16} opacity={0.45} />}
+        </label>
+        <div className="relative w-full">
+          <textarea
+            ref={ref}
+            className={cn(
+              'w-full rounded-lg border h-10 px-3 py-2 text-sm leading-none text-tertiary-foreground font-normal transition-all outline-none',
+              'placeholder:text-tertiary-foreground placeholder:opacity-65 placeholder:font-normal placeholder:text-sm',
+              { 'border-divider-error focus:border-divider-error focus:shadow-text-field-error': error },
+              { 'border-divider hover:border-divider-active focus:border-divider-highlighted focus:shadow-text-field': !error },
+              { 'border-divider-disabled hover:border-divider-disabled cursor-not-allowed': disabled },
+              'resize-none'
+            )}
+            style={{ height: defaultHeight }}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            disabled={disabled}
+            {...props}
+          />
+        </div>
+        {error && <span className="text-error-foreground text-xs ">{error}</span>}
+        {helperText && !error && <span className="text-secondary-foreground leading-4 text-xs opacity-85">{helperText}</span>}
       </div>
-      {error && <span className="text-error-foreground text-xs ">{error}</span>}
-      {helperText && !error && <span className="text-secondary-foreground leading-4 text-xs opacity-85">{helperText}</span>}
-    </div>
-  );
-};
+    );
+  }
+);
+
+TextArea.displayName = 'TextArea';
 
 export { TextArea };
--- src/components/text-field/text-field.tsx
+++ src/components/text-field/text-field.tsx
@@ -1,11 +1,11 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, forwardRef, ReactNode } from 'react';
 
 import { cn } from '@/lib/utils';
 
 import { ExclamationCircleIcon } from '@stash-ui/regular-icons';
 import { AsteriskIcon } from '@stash-ui/solid-icons';
 
-interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+interface TextFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'prefix'> {
   label?: string;
   error?: string;
   helperText?: string;
@@ -13,49 +13,42 @@ interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   required?: boolean;
 }
 
-const TextField = ({
-  label,
-  placeholder,
-  value,
-  onChange,
-  error,
-  helperText,
-  disabled,
-  className,
-  required,
-  prefix,
-  ...props
-}: TextFieldProps) => {
-  return (
-    <div className={cn('flex flex-col gap-1', className)}>
-      <label className={cn('text-tertiary-foreground text-xs font-semibold leading-4 flex items-center', { 'opacity-85': disabled })}>
-        {label}
-        {required && <AsteriskIcon color="#52525B" width={16} height={16} opacity={0.45} />}
-      </label>
-      <div className="relative w-full">
-        {prefix && <div className={cn('absolute left-2 top-1/2 -translate-y-1/2', { 'opacity-45': disabled })}>{prefix}</div>}
-        <input
-          className={cn(
-            'w-full rounded-lg border h-10 px-3 py-2 text-sm leading-none text-tertiary-foreground font-normal transition-all outline-none',
-            'placeholder:text-tertiary-foreground placeholder:opacity-65 placeholder:font-normal placeholder:text-sm',
-            { 'border-divider-error focus:border-divider-error focus:shadow-text-field-error': error },
-            { 'border-divider hover:border-divider-active focus:border-divider-highlighted focus:shadow-text-field': !error },
-            { 'border-divider-disabled hover:border-divider-disabled cursor-not-allowed': disabled },
-            { 'pl-10': prefix }
-          )}
-          type="text"
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          disabled={disabled}
-          {...props}
-        />
-        {error && <ExclamationCircleIcon className="w-6 h-6 text-red-500 absolute right-2 top-1/2 -translate-y-1/2" />}
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  ({ label, placeholder, value, onChange, error, helperText, disabled, className, required, prefix, ...props }, ref) => {
+    return (
+      <div className={cn('flex flex-col gap-1', className)}>
+        <label className={cn('text-tertiary-foreground text-xs font-semibold leading-4 flex items-center', { 'opacity-85': disabled })}>
+          {label}
+          {required && <AsteriskIcon color="#52525B" width={16} height={16} opacity={0.45} />}
+        </label>
+        <div className="relative w-full">
+          {prefix && <div className={cn('absolute left-2 top-1/2 -translate-y-1/2', { 'opacity-45': disabled })}>{prefix}</div>}
+          <input
+            ref={ref}
+            className={cn(
+              'w-full rounded-lg border h-10 px-3 py-2 text-sm leading-none text-tertiary-foreground font-normal transition-all outline-none',
+              'placeholder:text-tertiary-foreground placeholder:opacity-65 placeholder:font-normal placeholder:text-sm',
+              { 'border-divider-error focus:border-divider-error focus:shadow-text-field-error': error },
+              { 'border-divider hover:border-divider-active focus:border-divider-highlighted focus:shadow-text-field': !error },
+              { 'border-divider-disabled hover:border-divider-disabled cursor-not-allowed': disabled },
+              { 'pl-10': prefix }
+            )}
+            type="text"
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            disabled={disabled}
+            {...props}
+          />
+          {error && <ExclamationCircleIcon className="w-6 h-6 text-red-500 absolute right-2 top-1/2 -translate-y-1/2" />}
+        </div>
+        {error && <span className="text-error-foreground text-xs mt-1">{error}</span>}
+        {helperText && !error && <span className="text-secondary-foreground leading-4 text-xs mt-1 opacity-85">{helperText}</span>}
       </div>
-      {error && <span className="text-error-foreground text-xs mt-1">{error}</span>}
-      {helperText && !error && <span className="text-secondary-foreground leading-4 text-xs mt-1 opacity-85">{helperText}</span>}
-    </div>
-  );
-};
+    );
+  }
+);
+
+TextField.displayName = 'TextField';
 
 export { TextField };
