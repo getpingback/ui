605b7c0463980906f16d36056db9e12c1f31c3d7
Jéss Martins <81935519+Jessmartins91@users.noreply.github.com>
2025-06-06 16:08:32 -0300

    fix: animation

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -10,8 +10,7 @@ interface ModalProps extends Dialog.DialogProps {
 const Modal = ({ children, className, ...props }: ModalProps) => (
   <Dialog.Root {...props}>
     <Dialog.Portal>
-      <Dialog.Overlay className="z-50 fixed inset-0 bg-[#FFFFFF]/70 w-screen h-screen backdrop-blur-[8px] animate-fade-in" />
-      <div className="fixed inset-0 z-50 flex items-end md:items-center justify-center">
+      <Dialog.Overlay className="z-50 grid md:place-items-center place-items-end grid-cols-1 fixed inset-0 bg-[#FFFFFF]/70 w-screen h-screen backdrop-blur-[8px] animate-fade-in">
         <Dialog.Content
           className={cn(
             'flex flex-col bg-[#FFFFFF] h-fit shadow-modal-large md:rounded-xl rounded-t-xl w-full md:w-auto md:max-w-xl p-4 data-[state=open]:animate-modal-slide-up md:data-[state=open]:animate-modal-fade-in data-[state=closed]:animate-modal-slide-down md:data-[state=closed]:animate-modal-fade-out',
@@ -20,7 +19,7 @@ const Modal = ({ children, className, ...props }: ModalProps) => (
         >
           {children}
         </Dialog.Content>
-      </div>
+      </Dialog.Overlay>
     </Dialog.Portal>
   </Dialog.Root>
 );
