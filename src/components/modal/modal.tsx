f0914aecc70f9b80e92129563179031a3d0b1e2b
Jéss Martins <81935519+Jessmartins91@users.noreply.github.com>
2025-06-06 14:35:19 -0300

    fix: conflict between modal content and select

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -11,14 +11,18 @@ const Modal = ({ children, className, ...props }: ModalProps) => (
   <Dialog.Root {...props}>
     <Dialog.Portal>
       <Dialog.Overlay className="z-50 fixed inset-0 bg-[#FFFFFF]/70 w-screen h-screen backdrop-blur-[8px] animate-fade-in" />
-      <Dialog.Content
-        className={cn(
-          'z-50 flex flex-col bg-[#FFFFFF] h-fit shadow-modal-large md:rounded-xl rounded-t-xl w-full md:w-auto md:max-w-xl p-4 fixed left-1/2 bottom-0 md:top-1/2 -translate-x-1/2 md:-translate-y-1/2 data-[state=open]:animate-modal-slide-up md:data-[state=open]:animate-modal-fade-in data-[state=closed]:animate-modal-slide-down md:data-[state=closed]:animate-modal-fade-out',
-          className
-        )}
-      >
-        {children}
-      </Dialog.Content>
+      <div className="z-50 fixed inset-0 overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center">
+          <Dialog.Content
+            className={cn(
+              'z-50 flex flex-col bg-[#FFFFFF] h-fit shadow-modal-large md:rounded-xl rounded-t-xl w-full md:w-auto md:max-w-xl p-4 relative data-[state=open]:animate-modal-slide-up md:data-[state=open]:animate-modal-fade-in data-[state=closed]:animate-modal-slide-down md:data-[state=closed]:animate-modal-fade-out',
+              className
+            )}
+          >
+            {children}
+          </Dialog.Content>
+        </div>
+      </div>
     </Dialog.Portal>
   </Dialog.Root>
 );
