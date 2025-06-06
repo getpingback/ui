6005008a57b21946c14ebf34b1ca8ba5768ecadd
Jéss Martins <81935519+Jessmartins91@users.noreply.github.com>
2025-06-06 15:13:28 -0300

    fix: modal position

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -11,17 +11,15 @@ const Modal = ({ children, className, ...props }: ModalProps) => (
   <Dialog.Root {...props}>
     <Dialog.Portal>
       <Dialog.Overlay className="z-50 fixed inset-0 bg-[#FFFFFF]/70 w-screen h-screen backdrop-blur-[8px] animate-fade-in" />
-      <div className="z-50 fixed inset-0 overflow-y-auto">
-        <div className="flex min-h-full items-center justify-center">
-          <Dialog.Content
-            className={cn(
-              'z-50 flex flex-col bg-[#FFFFFF] h-fit shadow-modal-large md:rounded-xl rounded-t-xl w-full md:w-auto md:max-w-xl p-4 relative data-[state=open]:animate-modal-slide-up md:data-[state=open]:animate-modal-fade-in data-[state=closed]:animate-modal-slide-down md:data-[state=closed]:animate-modal-fade-out',
-              className
-            )}
-          >
-            {children}
-          </Dialog.Content>
-        </div>
+      <div className="fixed inset-0 z-50 flex items-end md:items-center justify-center">
+        <Dialog.Content
+          className={cn(
+            'flex flex-col bg-[#FFFFFF] h-fit shadow-modal-large md:rounded-xl rounded-t-xl w-full md:w-auto md:max-w-xl p-4 data-[state=open]:animate-modal-slide-up md:data-[state=open]:animate-modal-fade-in data-[state=closed]:animate-modal-slide-down md:data-[state=closed]:animate-modal-fade-out',
+            className
+          )}
+        >
+          {children}
+        </Dialog.Content>
       </div>
     </Dialog.Portal>
   </Dialog.Root>
