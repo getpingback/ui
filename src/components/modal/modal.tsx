d68687ef361442750ff277d72c6c16d441da6bae
roger067 <rosestolato.rogerio@gmail.com>
2025-06-09 11:07:35 -0300

    fix: overlay z-index

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps extends Dialog.DialogProps {
 const Modal = ({ children, className, ...props }: ModalProps) => (
   <Dialog.Root {...props}>
     <Dialog.Portal>
-      <Dialog.Overlay className="z-50 fixed inset-0 bg-[#FFFFFF]/70 w-screen h-screen backdrop-blur-[8px] animate-fade-in" />
+      <Dialog.Overlay className="z-[45] fixed inset-0 bg-[#FFFFFF]/70 w-screen h-screen backdrop-blur-[8px] animate-fade-in" />
       <Dialog.Content
         className={cn(
           'z-50 flex flex-col bg-[#FFFFFF] h-fit shadow-modal-large md:rounded-xl rounded-t-xl w-full md:w-auto md:max-w-xl p-4 fixed left-1/2 bottom-0 md:top-1/2 -translate-x-1/2 md:-translate-y-1/2 data-[state=open]:animate-modal-slide-up md:data-[state=open]:animate-modal-fade-in data-[state=closed]:animate-modal-slide-down md:data-[state=closed]:animate-modal-fade-out',
