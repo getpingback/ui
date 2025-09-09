d093909114f5ece927115b08ff7ec7b6cb619af2
roger067 <rosestolato.rogerio@gmail.com>
2025-09-09 12:57:30 -0300

    refactor: dropzone

--- src/components/dropzone/dropzone.tsx
+++ src/components/dropzone/dropzone.tsx
@@ -1,82 +1,66 @@
 import React, { useState } from 'react';
-import { ImageArrowUpIcon } from '@stash-ui/duotone-icons';
-import { ImageSwitchIcon, ImageMinusIcon } from '@stash-ui/regular-icons';
-import {
-  formatFileSize,
-  handleFormatFile,
-  dropImage,
-  handleDragOver,
-} from '@/hooks/useFileHandler';
+import { formatFileSize, handleFormatFile, dropImage, handleDragOver } from '@/hooks/useFileHandler';
+import { ArrowsSwitchIcon, CloudArrowUpIcon, MinusIcon } from '@stash-ui/light-icons';
+import { FolderPlusIcon } from '@stash-ui/solid-icons';
+import { Button } from '../button';
 import { cn } from '@/lib/utils';
-
-const labels = {
-  'pt-br': {
-    dropYourImageHere: 'Arraste sua imagem aqui, ou <u>busque</u>',
-    pngJpgGif: 'PNG, JPG, JPEG e GIF, limite de tamanho de 5 MB.',
-    change: 'Alterar',
-    remove: 'Remover',
-  },
-  en: {
-    dropYourImageHere: 'Drop your image here, or <u>browse</u>',
-    pngJpgGif: 'PNG, JPG, JPEG and GIF, max 5 MB size limit.',
-    change: 'Change',
-    remove: 'Remove',
-  },
-  es: {
-    dropYourImageHere: 'Suelta tu imagen aquí, o <u>navegar</u>',
-    pngJpgGif: 'PNG, JPG, JPEG y GIF, límite de tamaño de 5 MB.',
-    change: 'Cambiar',
-    remove: 'Eliminar',
-  },
-};
+import { Spinner } from '../spinner';
 
 export interface DropzoneProps {
-  uploadedFile?: { name: string; size: number; src: string };
-  locale?: 'pt-br' | 'en' | 'es';
+  isLoading?: boolean;
+  accept?: string;
+  messages: {
+    title: string;
+    description: string;
+    primaryButton: string;
+    removeButton: string;
+  };
+  uploadedFile?: { name: string; size: number; src: string; type: string };
   onChange?: (
     file:
       | {
           type: 'upload' | 'remove';
-          file: { name: string; size: number; src: string | '' };
+          file: { name: string; size: number; src: string | ''; type: string };
         }
       | {}
   ) => void;
 }
 
-function Dropzone({ uploadedFile, onChange, locale = 'en' }: DropzoneProps) {
-  const [file, setFile] = useState(uploadedFile);
-
-  const avatarStyle = {
-    backgroundImage: `url(${file?.src})`,
-    width: 132,
-    height: 132,
-    minWidth: 132,
-    minHeight: 132,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    borderRadius: 8,
-    border: '1px solid #27272A14',
+interface PreviewProps {
+  file: { name: string; size: number; src: string; type: string };
+  messages: {
+    title: string;
+    description: string;
+    primaryButton: string;
+    removeButton: string;
   };
+  handleUploadFile: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  removeFile: () => void;
+  accept: string;
+  isLoading?: boolean;
+}
+
+function Dropzone({ uploadedFile, onChange, messages, isLoading, accept = 'image/*' }: DropzoneProps) {
+  const [file, setFile] = useState(uploadedFile);
 
   const handleDrop = (e: React.DragEvent<HTMLLabelElement>) => {
     const event = dropImage(e) as React.ChangeEvent<HTMLInputElement>;
     handleUploadFile(event);
   };
 
-  const handleUploadFile = async (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleUploadFile = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = (await handleFormatFile(event)) as {
       name: string;
       size: number;
       src: string;
+      type: string;
     };
 
     setFile(file);
     onChange &&
       onChange({
         type: 'upload',
-        file: file,
+        file: file
       });
   };
 
@@ -85,126 +69,102 @@ function Dropzone({ uploadedFile, onChange, locale = 'en' }: DropzoneProps) {
     onChange &&
       onChange({
         type: 'remove',
-        file: {},
+        file: {}
       });
   };
 
-  const ImagePreview = () => {
-    const Button = ({
-      onClick,
-      className,
-      children,
-      ...props
-    }: {
-      onClick?: () => void;
-      className?: string;
-      children: React.ReactNode;
-    }) => (
-      <button
-        onClick={onClick}
-        className={cn(
-          'w-full flex items-center justify-center rounded-lg h-[32px] border border-[#D4D4D8] py-[8px] px-[12px] text-secondary-foreground text-xs font-semibold hover:shadow-[0_0_0_3px_#71717A12] transition-all',
-          className
-        )}
-        {...props}
-      >
-        {children}
-      </button>
-    );
-
-    const renderFileName = (fileName: string) => {
-      if (fileName.length <= 100) {
-        return fileName;
-      } else {
-        const extensionName = fileName.split('.').pop();
-        const name = fileName.substring(0, fileName.lastIndexOf('.'));
-        const formattedName =
-          extensionName && name.substring(0, 100 - extensionName.length - 3);
-        return formattedName + '...' + extensionName;
-      }
-    };
-
-    return (
-      <div
-        className='flex justify-between h-[132px]'
-        data-testid='image-preview'
-      >
-        <div data-testid='image' style={avatarStyle} />
-
-        <div className='w-full flex flex-col justify-between pl-[12px]'>
-          <div>
-            {file?.name && (
-              <p className='line-clamp-3 break-all text-sm font-semibold text-primary-foreground'>
-                {renderFileName(file?.name)}
-              </p>
-            )}
-            {file?.size && (
-              <p className='text-xs font-normal text-[#A1A1AA]'>
-                {formatFileSize(file?.size)}
-              </p>
-            )}
-          </div>
-
-          <div className='flex'>
-            <Button className='mr-[12px]'>
-              <label
-                htmlFor='file-upload'
-                className='flex items-center cursor-pointer'
-              >
-                <ImageSwitchIcon width={16} height={16} className='mr-[4px]' />
-                {labels[locale].change}
-                <input
-                  id='file-upload'
-                  type='file'
-                  accept='image/*'
-                  className='hidden'
-                  onChange={(e) => handleUploadFile(e)}
-                />
-              </label>
-            </Button>
-            <Button onClick={removeFile} data-testid='remove-btn'>
-              <ImageMinusIcon width={16} height={16} className='mr-[4px]' />
-              {labels[locale].remove}
-            </Button>
-          </div>
-        </div>
-      </div>
-    );
-  };
-
-  const DropArea = () => (
-    <label
-      data-testid='drop-area'
-      onDragOver={handleDragOver}
-      onDrop={handleDrop}
-      htmlFor='file-upload'
-      className='flex justify-center items-center flex-col border-2 border-dashed p-[24px] rounded-xl [&>svg>path]:hover:fill-[#9061F9] [&>svg>g]:hover:fill-[#9061F9] hover:border-[#9061F973] transition-all cursor-pointer'
-    >
-      <ImageArrowUpIcon className='[&>path]:fill-[#71717A] [&>path>g]:fill-[#71717A]' />
-      <p
-        className='text-sm font-normal text-secondary-foreground'
-        dangerouslySetInnerHTML={{
-          __html: labels[locale].dropYourImageHere,
-        }}
-      />
-      <p className='text-xs font-normal text-tertiary-foreground'>
-        {labels[locale].pngJpgGif}
-      </p>
-      <input
-        id='file-upload'
-        type='file'
-        accept='image/*'
-        className='hidden'
-        onChange={handleUploadFile}
-      />
-    </label>
-  );
-
   return (
-    <div className='w-full' data-testid='dropzone'>
-      {file ? <ImagePreview /> : <DropArea />}
+    <div
+      className="flex items-center justify-center border-2 border-dashed border-hover h-[180px] w-full max-w-[632px] rounded-2xl hover:border-filled transition-all cursor-pointer"
+      data-testid="dropzone"
+    >
+      {file || isLoading ? (
+        <Preview
+          file={file || { name: '', size: 0, src: '', type: '' }}
+          messages={messages}
+          handleUploadFile={handleUploadFile}
+          removeFile={removeFile}
+          accept={accept}
+          isLoading={isLoading}
+        />
+      ) : (
+        <label
+          className="w-full h-full flex items-center justify-center flex-col p-6 cursor-pointer"
+          data-testid="drop-area"
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+          htmlFor="file-upload"
+        >
+          <CloudArrowUpIcon className="[&>path]:fill-icon-tertiary mb-2 w-7 h-7" />
+          <p className="text-sm font-semibold text-tertiary leading-6">{messages.title}</p>
+          <p className="text-xs font-normal text-tertiary">{messages.description}</p>
+          <input id="file-upload" type="file" accept={accept} className="hidden" onChange={handleUploadFile} />
+        </label>
+      )}
     </div>
   );
 }
+
+const Preview = ({ file, messages, handleUploadFile, removeFile, accept, isLoading }: PreviewProps) => {
+  const isImage = file.type.startsWith('image/');
+
+  const avatarStyle = {
+    backgroundImage: `url(${file?.src})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  };
+
+  const renderFileName = (fileName: string) => {
+    if (fileName.length <= 100) {
+      return fileName;
+    } else {
+      const extensionName = fileName.split('.').pop();
+      const name = fileName.substring(0, fileName.lastIndexOf('.'));
+      const formattedName = extensionName && name.substring(0, 100 - extensionName.length - 3);
+      return formattedName + '...' + extensionName;
+    }
+  };
+
+  if (isLoading) {
+    return <Spinner size="large" variant="gray" />;
+  }
+
+  return (
+    <div className="flex w-fit items-center h-full gap-3 p-6" data-testid="image-preview">
+      {isImage && (
+        <div
+          className="w-32 h-32 lg:min-w-32 lg:min-h-32 min-w-20 min-h-20 rounded-2xl border border-neutral max-w-fit"
+          data-testid="image"
+          style={avatarStyle}
+        />
+      )}
+
+      <div className={cn('flex flex-col', { 'justify-between w-fit h-full': isImage, 'items-center h-fit gap-6': !isImage })}>
+        <div className={cn({ 'flex flex-col items-center gap-1': !isImage })}>
+          {file?.name && (
+            <div className="flex items-center gap-1">
+              {!isImage && <FolderPlusIcon className="text-icon-tertiary" />}
+              <p className="line-clamp-3 break-all text-sm leading-6 font-semibold text-tertiary">{renderFileName(file?.name)}</p>
+            </div>
+          )}
+          {file?.size && <p className="text-xs font-normal text-gray-400">{formatFileSize(file?.size)}</p>}
+        </div>
+
+        <div className="flex gap-2 w-fit">
+          <Button variant="outline" prefix={<ArrowsSwitchIcon />} data-testid="change-btn">
+            <label htmlFor="file-upload" className="flex items-center cursor-pointer">
+              {messages.primaryButton}
+              <input id="file-upload" type="file" accept={accept} className="hidden" onChange={(e) => handleUploadFile(e)} />
+            </label>
+          </Button>
+
+          <Button variant="outline" prefix={<MinusIcon />} onClick={removeFile} data-testid="remove-btn">
+            {messages.removeButton}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export { Dropzone };
