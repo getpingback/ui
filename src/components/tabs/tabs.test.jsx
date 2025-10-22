4eda8d10f5b8a5839c1826e7a2e6bd38b5962523
Jéss Martins <81935519+Jessmartins91@users.noreply.github.com>
2025-10-22 15:39:30 -0300

    fix: add tests to new tab variant

--- src/components/tabs/tabs.test.jsx
+++ src/components/tabs/tabs.test.jsx
@@ -6,9 +6,9 @@ import { composeStories } from '@storybook/testing-react';
 import * as stories from './tabs.stories';
 import exp from 'constants';
 
-const { Default } = composeStories(stories);
+const { Default, Secondary } = composeStories(stories);
 
-describe('Tabs Component', () => {
+describe('Tabs Component Default', () => {
   it('renders the Tabs component', () => {
     render(<Default />);
     expect(screen.getByRole('tablist')).toBeInTheDocument();
@@ -67,6 +67,67 @@ describe('Tabs Component', () => {
   });
   it('render the Tabs component with the correct active panel', async () => {
     render(<Default />);
+    const user = userEvent.setup();
+    const tabs = screen.getAllByTestId(/tabs-trigger/i);
+    const panels = screen.getAllByTestId(/tabs-content/i);
+    await user.click(tabs[1]);
+    expect(panels[1]).toHaveAttribute('data-state', 'active');
+    expect(panels[1]).toHaveTextContent('Tab 2');
+
+    await user.click(tabs[2]);
+    expect(panels[2]).toHaveAttribute('data-state', 'active');
+    expect(panels[2]).toHaveTextContent('Tab 3');
+
+    await user.click(tabs[3]);
+    expect(panels[3]).toHaveAttribute('data-state', 'active');
+    expect(panels[3]).toHaveTextContent('Tab 4');
+
+    await user.click(tabs[4]);
+    expect(panels[4]).toHaveAttribute('data-state', 'active');
+    expect(panels[4]).toHaveTextContent('Tab 5');
+  });
+});
+
+describe('Tabs Component Secondary', () => {
+  it('renders the Tabs component', () => {
+    render(<Secondary />);
+    expect(screen.getByRole('tablist')).toBeInTheDocument();
+  });
+  it('render the Tabs component with the correct active tab', async () => {
+    render(<Secondary />);
+    const user = userEvent.setup();
+    const tabs = screen.getAllByTestId(/tabs-trigger/i);
+    await user.click(tabs[1]);
+
+    expect(tabs[0]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[1]).toHaveAttribute('data-state', 'active');
+    expect(tabs[2]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[3]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[4]).toHaveAttribute('data-state', 'inactive');
+
+    await user.click(tabs[2]);
+    expect(tabs[0]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[1]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[2]).toHaveAttribute('data-state', 'active');
+    expect(tabs[3]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[4]).toHaveAttribute('data-state', 'inactive');
+
+    await user.click(tabs[3]);
+    expect(tabs[0]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[1]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[2]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[3]).toHaveAttribute('data-state', 'active');
+    expect(tabs[4]).toHaveAttribute('data-state', 'inactive');
+
+    await user.click(tabs[4]);
+    expect(tabs[0]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[1]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[2]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[3]).toHaveAttribute('data-state', 'inactive');
+    expect(tabs[4]).toHaveAttribute('data-state', 'active');
+  });
+  it('render the Tabs component with the correct active panel', async () => {
+    render(<Secondary />);
     const user = userEvent.setup();
     const tabs = screen.getAllByTestId(/tabs-trigger/i);
     const panels = screen.getAllByTestId(/tabs-content/i);
